e70b88c5692041ccc372f0c90ae55a70917ab35e
Corey Miller <Corey@Coreys-iMac.local>
2017-09-09 00:13:38 -0500

    First commit.

---
+++ server.js
@@ -0,0 +1,10 @@
+console.log('May Node be with you')
+const express = require('express');
+const app = express();
+app.listen(3000, function() {
+	console.log('listening on 3000')
+})
+app.get('/', (req, res) => {
+// 	res.send('hello world')
+	res.sendFile(__dirname + '/index.html')
+})
